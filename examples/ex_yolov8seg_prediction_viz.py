8629040ad7044fcb180fe581269eeab92d42df11
Josh <uvtoc@student.kit.edu>
2023-09-27 11:29:25 +0200

    BugFix area ratios

--- examples/ex_yolov8seg_prediction_viz.py
+++ examples/ex_yolov8seg_prediction_viz.py
@@ -37,4 +37,5 @@ for cls, mask in zip(predictions[0][0].boxes.cls.tolist(), bin_masks):
     segment_areas[labels[cls]].append(segment_area_obj)
 
 area_ratios = segment_area_comparison(segment_areas)
+area_ratios = area_ratios.iloc[0].to_dict()
 plot_grouped_ratio_barplot_with_labels(area_ratios, list(predictions[0][0].names.values()))
