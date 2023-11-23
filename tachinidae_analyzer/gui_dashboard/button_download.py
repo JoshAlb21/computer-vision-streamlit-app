04f68250da1c84b3fd67945cc258e2e1b30cdf6b
Josh <uvtoc@student.kit.edu>
2023-11-23 14:13:40 +0100

    New buttongs

--- tachinidae_analyzer/gui_dashboard/button_download.py
+++ tachinidae_analyzer/gui_dashboard/button_download.py
@@ -1,9 +1,13 @@
-from zipfile import ZipFile, ZIP_DEFLATED
 from io import BytesIO
 import base64
 import streamlit as st
 from datetime import datetime
 from typing import Dict
+import ultralytics
+import json
+import os
+
+import tachinidae_analyzer as ta
 
 
 def button_download_all(all_data:Dict[str, BytesIO]):
@@ -15,15 +19,35 @@ def button_download_all(all_data:Dict[str, BytesIO]):
             Example: {"test.csv": BytesIO(b"test,test,test")}
     '''
 
-    zip_buffer = BytesIO()
+    # Set title and check boxes
+    with st.sidebar:
+        st.write("## Download :gear:")
+        st.checkbox("json annotation", value=True, key="check_json_download")
+        st.checkbox("img w. segments", value=True, key="check_img_w_segm_download")
+        st.checkbox("img w. orth. lines", value=True, key="check_img_w_orth_lines_download")
+        st.checkbox("information csv", value=True, key="df_combined_download")
+    
+    # Prepare data to download
+    data_to_keep = {}
+    data_to_keep['cog_body_parts.csv'] = all_data['cog_body_parts.csv']
+    if st.session_state.check_img_w_segm_download:
+        data_to_keep["segmented_image.png"] = all_data["segmented_image.png"]
+    if st.session_state.check_img_w_orth_lines_download:
+        data_to_keep["volume_estimation_img.png"] = all_data["volume_estimation_img.png"]
+    if st.session_state.check_json_download:
+        #data_to_keep["annotations.json"] = all_data["annotations"]
+        data_to_keep["annotations.zip"] = all_data["annotations.zip"]
+    if st.session_state.df_combined_download:
+        data_to_keep["information.csv"] = all_data["information.csv"]
 
-    with ZipFile(zip_buffer, "a", ZIP_DEFLATED, False) as zip_file:
-        for file_name, data in all_data.items():
-            zip_file.writestr(file_name, data.getvalue())
+    # Prepare multiple files as a zip file
+    zip_buffer = ta.gui_dashboard.utils.save_as_zip.buffer_in_zip(data_to_keep)
 
-    st.download_button(
-        "Download csv", 
-        file_name=f"results_{datetime.now()}.zip", 
-        mime="application/zip", 
-        data=zip_buffer
-    )
+    with st.sidebar:
+        st.sidebar.write("CoG for each body part are downloaded by default. Select further files to download.")
+        st.download_button(
+            "Download csv", 
+            file_name=f"results_{datetime.now()}.zip", 
+            mime="application/zip", 
+            data=zip_buffer
+        )
--- tachinidae_analyzer/gui_dashboard/button_model_selection.py
+++ tachinidae_analyzer/gui_dashboard/button_model_selection.py
@@ -7,4 +7,14 @@ def model_selector(folder_path='.'):
     except (NotADirectoryError, FileNotFoundError) as e:
         return None
     selected_filename = st.selectbox('Select a model', filenames)
-    return os.path.join(folder_path, selected_filename)
+
+    if selected_filename is None:
+        model_path = None
+    else:
+        if selected_filename.endswith(".pt") or selected_filename.endswith(".pth"):
+            model_path = os.path.join(folder_path, selected_filename)
+        else:
+            model_path = None
+            st.error("Please select a valid model file (.pt or .pth)")
+            
+    return model_path
