7d06d3e601d2c880d50a1b31271a101e8b7b2ff0
Josh <uvtoc@student.kit.edu>
2023-12-13 12:28:03 +0100

    Update bash and shell scripts to cover edge cases in case a container with same name or port is already running

--- start_app.bat
+++ start_app.bat
@@ -1,12 +1,56 @@
 @echo off
-
-:: Run the Docker container
-docker run -p 8501:8501 tachinidae_analyzer_image
-IF %ERRORLEVEL% NEQ 0 (
-    echo Failed to run the Docker container.
-    echo Please ensure the Docker image is built. If not, run 'install_app.bat' first.
-    exit /b
+REM Check if Docker is running
+docker info > NUL 2>&1
+if %errorlevel% neq 0 (
+    echo Docker is not running. Please start Docker and then rerun this script.
+    exit /b 1
 )
 
-:: Open the browser if the container starts successfully
-start "" http://localhost:8501
+REM Docker container name
+set CONTAINER_NAME=tachinidae_analyzer
+
+REM Check if a Docker container with the specified name is running or exists
+docker ps -a | findstr /C:%CONTAINER_NAME% > NUL
+if %errorlevel% neq 0 (
+    call :run_docker_container
+    call :is_container_running
+    if %errorlevel% eq 0 (
+        echo Container is running.
+        call :open_streamlit_app
+    ) else (
+        echo Failed to start the container. Please check Docker logs.
+    )
+    goto :EOF
+)
+
+REM Handle the existing container
+echo A container with the name '%CONTAINER_NAME%' already exists.
+set /p user_choice=Do you want to remove the existing container and start a new one? (y/n): 
+
+if /I "%user_choice%"=="y" (
+    docker rm -f %CONTAINER_NAME%
+    call :run_docker_container
+    echo Opening the Streamlit app in the new container...
+    call :open_streamlit_app
+) else (
+    echo Opening the Streamlit app in the existing container...
+    call :open_streamlit_app
+)
+goto :EOF
+
+:run_docker_container
+echo Running the Docker container...
+docker run -d --name %CONTAINER_NAME% -p 8501:8501 tachinidae_analyzer_image
+REM Wait briefly for the container to start
+timeout /t 5 /nobreak
+echo Waited 5 seconds.
+goto :EOF
+
+:is_container_running
+docker ps | findstr /C:%CONTAINER_NAME% > NUL
+exit /b %errorlevel%
+
+:open_streamlit_app
+echo Opening the Streamlit app in the browser...
+start http://localhost:8501
+goto :EOF
--- start_app.sh
+++ start_app.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
+# Function to check if Docker is running
+is_docker_running() {
+    docker info > /dev/null 2>&1
+    return $?
+}
+
 # Docker container name
 CONTAINER_NAME="tachinidae_analyzer"
+
+# Function to check if a Docker container with the specified name is running or exists
+check_container_exists() {
+    docker ps -a | grep -q $CONTAINER_NAME
+    return $?
+}
+
+# Function to prompt the user and handle the existing container
+handle_existing_container() {
+    echo "A container with the name '$CONTAINER_NAME' already exists."
+    read -p "Do you want to remove the existing container and start a new one? (y/n): " user_choice
+
+    if [[ $user_choice == "y" ]]; then
+        docker rm -f $CONTAINER_NAME
+        run_docker_container
+        echo "Opening the Streamlit app in the new container..."
+        open_streamlit_app
+    else
+        echo "Opening the Streamlit app in the existing container..."
+        open_streamlit_app
+    fi
+}
 
 # Function to check if the specified port is being used by a Docker container
 check_and_stop_container_on_port() {
@@ -20,20 +48,50 @@ check_and_stop_container_on_port() {
 # Function to run the Docker container
 run_docker_container() {
     echo "Running the Docker container..."
-    docker run --name $CONTAINER_NAME -p 8501:8501 tachinidae_analyzer_image &
+    docker run -d --name $CONTAINER_NAME -p 8501:8501 tachinidae_analyzer_image
+    # Wait briefly for the container to start
+    sleep 5
+    echo "Waited 5 seconds."
+}
+
+# Function to check if the container is running
+is_container_running() {
+    docker ps | grep -q $CONTAINER_NAME
+    return $?
 }
 
 # Function to open the Streamlit app in the browser
 open_streamlit_app() {
     echo "Opening the Streamlit app in the browser..."
-    open http://localhost:8501
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        xdg-open http://localhost:8501
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        open http://localhost:8501
+    elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]]; then
+        start http://localhost:8501
+    else
+        echo "Unable to detect OS type. Please manually open http://localhost:8501 in your browser."
+    fi
 }
 
 # Main script execution
+if ! is_docker_running; then
+    echo "Docker is not running. Please start Docker and then rerun this script."
+    exit 1
+fi
+
 if check_and_stop_container_on_port 8501; then
     sleep 2 # Wait for the container to fully stop
 fi
 
-run_docker_container
-sleep 10 # Wait for the server to start
-open_streamlit_app
+if check_container_exists; then
+    handle_existing_container
+else
+    run_docker_container
+    if is_container_running; then
+        echo "Container is running."
+        open_streamlit_app
+    else
+        echo "Failed to start the container. Please check Docker logs."
+    fi
+fi
